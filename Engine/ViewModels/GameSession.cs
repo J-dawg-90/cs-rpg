5cc97fba83016b99fbcca8bcb1d50f49c38bced0
Jon Walker <jondwalker1990@gmail.com>
2020-05-31 01:32:57 -0400

    lesson 4.1

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -10,16 +10,27 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+
         public GameSession()
         {
             CurrentPlayer = new Player
             {
-                Name = "Jonathan",                
+                Name = "Jonathan",
                 CharacterClass = "Fighter",
                 HitPoints = 10,
                 Gold = 1000000,
                 ExperiencePoints = 0,
                 Level = 1
+            };
+
+            CurrentLocation = new Location
+            {
+                Name = "Home",
+                XCoordinate = 0,
+                YCoordinate = -1,
+                Description = "This is your house",
+                ImageName = "/Engine;component/Images/Locations/Home.png"
             };
         }
     }
